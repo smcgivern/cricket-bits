44484a872d156978c8c1c72517e9ebb919e312da
Sean McGivern <sean@mcgivern.me.uk>
2024-10-19 17:55:48 +0100

    Add more teams, find players on index page

--- england-domestic-players/scrape.rb
+++ england-domestic-players/scrape.rb
@@ -27,12 +27,39 @@ TEAMS = [
   387, # Warwickshire
   399, # Worcestershire
   404, # Yorkshire
+  39506, # Lancashire Thunder
   46406, # Thunder
+  39054, # Loughborough Lightning
   46405, # Lightning
+  50235, # The Blaze
   39052, # Vipers
+  39055, # Surrey Stars
   46404, # Stars
   39053, # Storm
-  46408, # Diamonds
+  2971, # Diamonds
+  39051, # Yorkshire Diamonds
+  46408, # Northern Diamonds
+  46409, # Sunrisers
+  46407, # Central Sparks
+  20, # Australia
+  23, # Australia women
+  523, # Bangladesh
+  4597, # Bangladesh women
+  796, # India
+  806, # India women
+  992, # New Zealand
+  1008, # New Zealand women
+  263, # Pakistan
+  1098, # Pakistan women
+  1235, # South Africa
+  1244, # South Africa women
+  320, # Sri Lanka
+  1285, # Sri Lanka women
+  1356, # West Indies
+  1364, # West Indies women
+  407, # Zimbabwe
+  3613, # Zimbabwe women
+
 ]
 
 def fetch(url)
@@ -49,6 +76,20 @@ def fetch(url)
   end
 end
 
+def find_players(team, page)
+  page.css('#columnLeft td a').each do |player_link|
+    href = player_link.attr('href').value
+    name = player_link.text.strip
+    url = "#{BASE}#{href}"
+
+    next unless href.include?('/Players/')
+    next if name.include?('of the Year')
+
+    PLAYERS[url] ||= { name: name, teams: [] }
+    PLAYERS[url][:teams] << team
+  end
+end
+
 TEAMS.each do |id|
   dir = id / 1000
   team_base = "#{BASE}/Archive/Teams/#{dir}/#{id}/"
@@ -58,21 +99,18 @@ TEAMS.each do |id|
            .text
            .gsub(' Players', '')
            .gsub('Players who have played for ', '')
-           .gsub(/ (England|Wales)/, '')
+           .gsub(/ \((England|Wales)\)/, '')
 
-  index.css('#columnLeft center a').each do |player_index_link|
-    href = player_index_link.attr('href').value
-    Oga
-      .parse_html(fetch("#{href.start_with?('/') ? BASE : team_base}#{href}"))
-      .css('#columnLeft td a')
-      .each do |player_link|
-        next unless player_link.attr('href').value.include?('/Players/')
-        next if player_link.text.include?('of the Year')
-        name = player_link.text.strip
-        url = "#{BASE}#{player_link.attr('href')}"
-        PLAYERS[url] ||= { name: name, teams: [] }
-        PLAYERS[url][:teams] << team
-      end
+  if index.css('#columnLeft center>a').length == 0
+    find_players(team, index)
+  else
+    index.css('#columnLeft center>a').each do |player_index_link|
+      href = player_index_link.attr('href').value
+      find_players(
+        team,
+        Oga.parse_html(fetch("#{href.start_with?('/') ? BASE : team_base}#{href}"))
+      )
+    end
   end
 end
 
